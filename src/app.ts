0f1441588f02c4649c04389becd2d4bd15e3b22f
habibb2r <habibhk127@gmail.com>
2025-04-08 20:10:22 +0600

    added cors with cookie parser

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import { ProductRoutes } from './Modules/Products/products.route';
 import { OrderRoutes } from './Modules/Order/order.route';
 import AuthRouter from './Modules/Auth/auth.route';
 const app: Application = express();
-
+import cookieParser from 'cookie-parser'
 //parser
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173','http://localhost:5000',], credentials: true }));
 
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
