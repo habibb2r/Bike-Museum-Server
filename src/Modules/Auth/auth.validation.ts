f3aa547a7b54103706036a66ba4b2b391ece74e2
TahmidTausif <tahmidtausif49@gmail.com>
2025-04-13 03:01:37 +0600

    Auth validation updated

--- src/Modules/Auth/auth.validation.ts
+++ src/Modules/Auth/auth.validation.ts
@@ -1,23 +1,24 @@
 import { z } from "zod";
 
 const loginValidationSchema = z.object({
-      email: z.string({ required_error: 'Email is required.' }).email(),
-      password: z.string({ required_error: 'Password is required' }),
+  email: z.string({ required_error: 'Email is required.' }).email(),
+  password: z.string({ required_error: 'Password is required' }),
 
-  });
+});
 
-  const RegisterValidationSchema = z.object({
-    name: z.string({ required_error: 'Name is required.' }),
-    email: z
-      .string({ required_error: 'Email is required.' })
-      .email('Invalid email format'),
-    password: z.string({ required_error: 'Password is required.' }),
-    role: z.enum(['admin', 'user']).default('user'),
-    isBlocked: z.boolean().default(false),
-  });
+const RegisterValidationSchema = z.object({
+  name: z.string({ required_error: 'Name is required.' }),
+  email: z
+    .string({ required_error: 'Email is required.' })
+    .email('Invalid email format'),
+  password: z.string({ required_error: 'Password is required.' }),
+  imageUrl: z.string({ required_error: 'Image URL is required.' }).url('Invalid image URL'),
+  role: z.enum(['admin', 'user']).default('user'),
+  isBlocked: z.boolean().default(false),
+});
 
 
-  export const AuthValidation = { 
-    loginValidationSchema,
-    RegisterValidationSchema
-   };
+export const AuthValidation = {
+  loginValidationSchema,
+  RegisterValidationSchema
+};
